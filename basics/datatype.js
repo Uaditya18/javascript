801d1a6f1398fdc7177b7ae8b213452fd299a2ff
Aditya Upadhyay <120381566+Uaditya18@users.noreply.github.com>
2024-08-22 19:21:34 +0000

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict"   /* a statement to declare to use newwr version of js
+ although there is no need to put it haere node js atomatically use latest version but good to use*/
+
+
+//  alert("hello") can't be done in nodejs can be done in browser
+
+
+let name = "aditya" //string
+let age = 18 //number
+let isloggedIn = true //boolean
+let saySomething = null // nul -> a stade alone datatype define the variable is empty not filled with any thing
+let willSay; //undefined means will be defined later
+//symbol -> used to define the uniqueness of any component
+
+
+
+// OBJECT 
+console.log(typeof "Aditya");
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,22 @@
 console.log("Aditya Upadhyay")
+const accountId = 123455
+let accountEmail = "uaditya34562@gmail.com"
+var accountPassword = "12nfs"
+accountCity ="Jaipur"
+
+
+//accountId = 2435;  so we cannot change it 
+
+console.log(accountId);
+
+accountEmail= "hello@gmail.com"
+accountPassword = "mgoregre"
+accountCity = "shimla"
+
+console.table([accountEmail,accountPassword,accountCity])
+
+/*
+
+prefer not to use "var" keyword for variable because of 
+working block and functional block error
+*/
