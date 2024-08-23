1f509e09b52f95c64a141f00f5325ee2a981b28f
Aditya Upadhyay <120381566+Uaditya18@users.noreply.github.com>
2024-08-23 16:31:47 +0000

    datatype conversion

---
+++ basics/datatypeconversion.js
@@ -0,0 +1,19 @@
+// let score = 33
+// console.log(typeof score);
+// let valueInNumber = Number(score) //type conversion is happening here
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// null => 0
+
+// now for boolean values
+
+//  let isLoggedIn = 2
+//  let isBoolean = Boolean(isLoggedIn)
+//  console.log(isBoolean);
+ 
+//  1 => true 0=> false
+// "",null,undefined => false
+//  anything taht is not 0 in true 
